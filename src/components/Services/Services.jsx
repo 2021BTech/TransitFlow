0b8d2d88edfb0d568fc4e159f2a8d2b4ce3060bb
2021BTech <daffydanni@gmail.com>
2023-03-31 10:03:43 +0100

    still working

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,69 +1,53 @@
-// import React from 'react'
-// import "./Services.modules.css"
+import React from 'react'
+import "./Services.modules.css"
 //img
-// import Bg from "../../assets/Photo.png"
-// import mid from "../../assets/bl.jpg"
-// import min from "../../assets/service.png"
-// import AV1 from "../../assets/service-1.png"
-// import AV2 from "../../assets/service-2.png"
-// import AV3 from "../../assets/service-3.png"
-// import AV4 from "../../assets/service-4.png"
-// import AV5 from "../../assets/service-5.png"
+import Bg from "../../assets/Photo.png"
+import mid from "../../assets/service.png"
+import AV1 from "../../assets/service-1.png"
+import AV2 from "../../assets/service-2.png"
+import AV3 from "../../assets/service-3.png"
+import AV4 from "../../assets/service-4.png"
+import AV5 from "../../assets/service-5.png"
 //icon
-// import {AiOutlineDeliveredProcedure} from "react-icons/ai"
-// import {FaRegMoneyBillAlt} from "react-icons/fa"
+import {AiOutlineDeliveredProcedure} from "react-icons/ai"
+import {FaRegMoneyBillAlt} from "react-icons/fa"
 
-// const Services = () => {
-//   return (
-//     <div className='service_container'>
-   
-
-    /* <div className='service_section'>
-      <div className='img_top'></div>
-      <div className='service_card'>
-        <div className='service_card_container'>
-          <h4 className='service_header'> Why Us </h4>
-          <h2> We provide full range global Logistic Solution</h2>
-          <p> 
-            Levarage agile framework to provide a robust synopsis for strategy
-            foster collaborative thinking to further  the overall value proposition.
-          </p>
-          <br/>
-          <p> 
-            Organically grow the holistic world view of distruptive
-            innovation via workplace diversity and empowerment.
-          </p>
-          <div className='icon_service_container'>
-          <div className='icon_service'>
-            <AiOutlineDeliveredProcedure/>
-            <h3> Delivery on Time</h3>
-          </div>
-          <div className='icon_service'>
-            <FaRegMoneyBillAlt/>
-            <h3> Optimize Travel Cost </h3>
-          </div>
-          </div>
-          <div className='card_img'>
-            <img src={mid} alt="" className='img_card'/>
-            <img src={min} alt="" className='img_card'/>
-          </div>
-        </div>
-          
-        <hr/>
-        <div className='service_counter'>
-          <div className='count'>
-            <h3>1294 </h3>
-            <p> Delivered Packages </p>
-          </div>
-          <div className='count'>
-            <h3>3594</h3>
-            <p> Satisfied Clients </p>
-          </div>
+const Services = () => {
+  return (
+    <div className='service_container'>
+      <img src={Bg} alt="pic" className="pic"/>
+      <div className="service_card">
+      <div className="service_card-header">
+        <h5 className="card-title">Why Us</h5>
+        <h2 className="card-subtitle">We provide full range global Logistic Solution</h2>
+      </div>
+      <div className="card-body">
+        <p className="card-text">
+          Levarage agile framework to provide a robust synopsis for strategy
+          foster collaborative thinking to further  the overall value proposition.
+        </p>
+        <ul className="card-list">
+          <li><AiOutlineDeliveredProcedure/>Delivery on Time</li>
+          <li><FaRegMoneyBillAlt/>Optimize Travel Cost</li>
+        </ul>
+        <div className="card-image">
+          <img src={mid} alt="picr" />
         </div>
       </div>
-    </div> */
-    
-      /* <h3 className='title_service'> Transporting Accross the World </h3>
+      <div className="card-footer">
+        <div>
+            <h3>1294 </h3>
+            <p className="card-footer-text"> Delivered Packages </p>
+        </div>
+        <div>
+            <h3>3594</h3>
+            <p className="card-footer-text"> Satisfied Clients </p>
+        </div>
+      </div>
+    </div>
+  
+    {/* bottom page */}
+      <h3 className='title_service'> Transporting Accross the World </h3>
       <div className='info_service'>
         <div className='img_bottom'></div>
         <div className='card_info'>
@@ -111,30 +95,3 @@
 }
 
 export default Services
-
-import React from 'react';
-import './Page.css';
-
-function Page() {
-  return (
-    <div className="page">
-      <img src="picture.jpg" alt="Picture" className="picture" />
-
-      <div className="card">
-        <div className="card-text">
-          <h2>Card Title</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum lectus auctor lacus auctor, ac consectetur urna ultrices. Sed id interdum ipsum.</p>
-        </div>
-        <div className="card-image">
-          <img src="card-image.jpg" alt="Card Image" />
-        </div>
-      </div>
-
-      <footer className="footer">
-        <p>Footer Text</p>
-      </footer>
-    </div>
-  );
-}
-
-export default Page; */
